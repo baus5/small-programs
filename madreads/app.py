0d990cd548df929ec1c5be2c0e86b400f021fb57
baus5 <65490597+baus5@users.noreply.github.com>
2023-01-18 19:20:17 +0300

    Reset

--- madreads/app.py
+++
@@ -1,9 +0,0 @@
-import club, the_matrix
-import random, sys
-
-if __name__ == "__main__":
-    myChoice = random.choice([the_matrix, club])
-    try:
-        myChoice.main()
-    except KeyboardInterrupt:
-        sys.exit()
